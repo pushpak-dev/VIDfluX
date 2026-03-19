7a511dbc343e3a628d3117f49039e8691f478d18
pushpak-dev <pushpak90208@gmail.com>
2026-03-19 15:25:42 +0530

    update2.2

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use("/api/stream", router);
 app.use(express.json());   
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/*", (req, res) => {
+app.get("*name", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 })   
 
